732fd69e5a3dd8d336cc524dea1675580297b157
NicoRomano <nicolasromano09218@gmail.com>
2026-08-29 14:35:20 -0300

    feat: se incorpora 'wildcard route' para pagina 404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,21 +5,23 @@ import { IngresoStock } from './pages/dashboard/dashboard-supervisor/ingreso-sto
 import { AjusteStock } from './pages/dashboard/dashboard-supervisor/ajuste-stock/ajuste-stock';
 import { ControlStock } from './shared/control-stock/control-stock';
 import { InicioDashboard } from './shared/inicio-dashboard/inicio-dashboard';
+import { Pagina404 } from './pages/pagina-404/pagina-404';
 
 
 export const routes: Routes = [
     {path: "", redirectTo: "sinertech/landing-page", pathMatch: 'full'},
     {path: "sinertech", component: MainLayout,
         children: [
-        {path: "dashboard-supervisor", component: DashboardSupervisor,
-            children: [
-                { path: '', redirectTo: 'inicio-dashboard', pathMatch: 'full'},
-                {path: 'inicio-dashboard', component: InicioDashboard, data: {tipoDashboard: 'supervisor'}},
-                {path: 'ingreso-stock', component: IngresoStock},
-                {path: 'ajuste-stock', component: AjusteStock},
-                {path: 'control-stock', component: ControlStock, data: {tipoDashboard: 'supervisor'}}
-                ]
-            }
+            {path: "dashboard-supervisor", component: DashboardSupervisor,
+                children: [
+                    { path: '', redirectTo: 'inicio-dashboard', pathMatch: 'full'},
+                    {path: 'inicio-dashboard', component: InicioDashboard, data: {tipoDashboard: 'supervisor'}},
+                    {path: 'ingreso-stock', component: IngresoStock},
+                    {path: 'ajuste-stock', component: AjusteStock},
+                    {path: 'control-stock', component: ControlStock, data: {tipoDashboard: 'supervisor'}}
+                    ]
+                },
         ]
-        }
+    },
+    {path: "**", component: Pagina404}
     ]
