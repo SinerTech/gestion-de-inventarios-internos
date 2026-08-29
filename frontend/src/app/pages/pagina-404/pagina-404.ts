15819d96735c4c931eae6c2a4365b50fc4c0169f
NicoRomano <nicolasromano09218@gmail.com>
2026-08-29 14:30:45 -0300

    feat: se agrega componente pagina 404

---
+++ frontend/src/app/pages/pagina-404/pagina-404.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pagina-404',
+  styleUrl: './pagina-404.css',
+  templateUrl: './pagina-404.html',
+})
+export class Pagina404 {}
