791c3bf51a01616bce50a764ef8a46cd5e0e0ba4
NicoRomano <nicolasromano09218@gmail.com>
2026-08-29 15:33:00 -0300

    fix: se ajusta comportamiento de navegacion e iconos

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   imports: [RouterLink, RouterLinkActive],
@@ -9,10 +9,16 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class Header {
   logoSinertech: string = "logo/logo_icon.png";
+  // Estas funciones se reemplazaran a futuro, actualmente ofrecen solo simulación
   // Simulación dinamica del componente
-  estaAutenticado: boolean = true;
+  // Cambiar a true para observar el boton Cerrar Sesión en dashboards y probarlo dinamicamente
+  estaAutenticado: boolean = false;
   cerrarSesion(): void {
     this.estaAutenticado=!this.estaAutenticado
     alert("Cerrando Sesión")
   };
+  constructor(private router: Router) {}
+  get esDashboard(): boolean {
+      return this.router.url.includes('dashboard');
+  }
 }
