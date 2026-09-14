a31756c3f144a9ea69ca66115fe63f71c2145cb3
NicoRomano <nicolasromano09218@gmail.com>
2026-09-14 17:26:40 -0300

    refactor: se reorganizan servicios del dashboard y actualizan imports en los demas componentes

--- frontend/src/app/shared/control-stock/control-stock.ts
+++ frontend/src/app/shared/control-stock/control-stock.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { DashboardService } from '../../services/dashboard-service';
+import { DashboardService } from '../../services/dashboard/dashboard-service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
--- frontend/src/app/shared/inicio-dashboard/inicio-dashboard.ts
+++ frontend/src/app/shared/inicio-dashboard/inicio-dashboard.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { DashboardService } from '../../services/dashboard-service';
+import { DashboardService } from '../../services/dashboard/dashboard-service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
