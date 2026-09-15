50babb2dfb4c61f0df8a30fa7ae4173606fb940e
NicoRomano <nicolasromano09218@gmail.com>
2026-09-15 02:16:23 -0300

    refactor: se actualiza control stock para obtener datos vía HTTP

--- frontend/src/app/shared/control-stock/control-stock.ts
+++ frontend/src/app/shared/control-stock/control-stock.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard/dashboard-service';
 import { ActivatedRoute } from '@angular/router';
+import { Producto } from '../../models/producto.models';
+import { ProductoService } from '../../services/producto/producto-service';
+import { ConfiguracionDashboard } from '../../models/dashboard.models';
 
 @Component({
   imports: [],
@@ -8,50 +11,74 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './control-stock.css',
   templateUrl: './control-stock.html',
 })
-export class ControlStock {
-
-  configuracionActual: any;
-  productosFiltrados: any[] = [];
+export class ControlStock implements OnInit {
+  listaProductos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
   terminoBusqueda = '';
+  configuracionActual!: ConfiguracionDashboard;
 
   private servicioDashboard = inject(DashboardService);
   private ruta = inject(ActivatedRoute);
+  private productService = inject(ProductoService);
+  private cdr = inject(ChangeDetectorRef);
 
-  ngOnInit() {
-    const tipoDashboard =
-      this.ruta.snapshot.data['tipoDashboard'];
+  readonly estadoClases: Record<string, string> = {
+    Activo: 'bg-success',
+    Inactivo: 'bg-danger',
+    Suspendido: 'bg-warning',
+    };
 
-    this.configuracionActual =
-      this.servicioDashboard.obtenerConfiguracion(
-        tipoDashboard
-      );
+  ngOnInit(): void {
+    const tipoDashboard = this.ruta.snapshot.data['tipoDashboard'];
+    this.servicioDashboard.obtenerConfiguracion().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.configuracionActual = data[tipoDashboard];
+      },
+      error: (e) => 
+        console.error('Error al cargar información de configuración del dashboard', e),
+      complete: () =>{
+          this.cdr.detectChanges();
+          console.info('complete');
+        }
+      })
 
-    this.productosFiltrados = [
-      ...this.configuracionActual.productos
-    ];
+    this.productService.obtenerListaProductos().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.listaProductos = data;
+        this.productosFiltrados = [...this.listaProductos];
+      },
+      error: (e) => 
+        console.error('Error al cargar productos', e),
+      complete: () =>{
+          this.cdr.detectChanges();
+          console.info('complete');
+        }
+      })
   }
 
-  actualizarBusqueda(event: Event) {
+  actualizarBusqueda(event: Event): void {
     const input = event.target as HTMLInputElement;
 
     this.terminoBusqueda =
       input.value.trim().toLowerCase();
   }
 
-  buscarProducto(event: Event) {
+  buscarProducto(event: Event): void {
     event.preventDefault();
 
     if (!this.terminoBusqueda) {
       this.productosFiltrados = [
-        ...this.configuracionActual.productos
+        ...this.listaProductos
       ];
       return;
     }
 
     this.productosFiltrados =
-      this.configuracionActual.productos.filter(
-        (producto: any) =>
-          producto.nombre
+      this.listaProductos.filter(
+        (producto) =>
+          producto.nombreProducto
             .toLowerCase()
             .includes(this.terminoBusqueda) ||
           producto.sku
