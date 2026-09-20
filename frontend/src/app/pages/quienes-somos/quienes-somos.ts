40ece412267a3e1d6d46783577152b548811ddb1
francoquispe33 <francoquispe1133@gmail.com>
2026-09-19 23:55:34 -0300

    fix: Se corrige error de no cargar integrantes

--- frontend/src/app/pages/quienes-somos/quienes-somos.ts
+++ frontend/src/app/pages/quienes-somos/quienes-somos.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { QuienesSomosService } from '../../services/quienes-somos/quienes-somos-service';
 
 @Component({
@@ -10,11 +10,13 @@ import { QuienesSomosService } from '../../services/quienes-somos/quienes-somos-
 export class QuienesSomos {
   integrantes: any;
   private QuienesSomosService = inject(QuienesSomosService);
-  
+  private cdr = inject(ChangeDetectorRef);
+
   ngOnInit(): void {
     this.QuienesSomosService.obtenerMiembrosEquipo().subscribe({
       next: (data) => {
         this.integrantes = data;
+        this.cdr.markForCheck()
       },
       error: (err) => {
         console.error('Error al obtener los miembros:', err);
